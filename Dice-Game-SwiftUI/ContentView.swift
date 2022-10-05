34d7e80b6be93879d847fbc36bd1de29764ded44
jonasbergstrom <jontrian97@gmail.com>
2022-10-05 19:03:46 +0200

    Initial Commit

---
+++ Dice-Game-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dice-Game-SwiftUI
+//
+//  Created by Jonas Bergström on 2022-10-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dice-Game-SwiftUI/Dice_Game_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dice_Game_SwiftUIApp.swift
+//  Dice-Game-SwiftUI
+//
+//  Created by Jonas Bergström on 2022-10-05.
+//
+
+import SwiftUI
+
+@main
+struct Dice_Game_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
