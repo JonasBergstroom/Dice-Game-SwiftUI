e700c34d53534c5284ab2c40ff06934fbce3f135
jonasbergstrom <jontrian97@gmail.com>
2022-10-05 19:49:46 +0200

    Created a ZStack that shows the play board with a roll button

--- Dice-Game-SwiftUI/ContentView.swift
+++ Dice-Game-SwiftUI/ContentView.swift
@@ -8,11 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Image("DiceGameBG")
+                .resizable()
+                .ignoresSafeArea(.all)
+            VStack {
+                Image("DiceGameLogo")
+                    .resizable()
+                    .scaledToFit()
+                    Spacer()
+                HStack {
+                Image("dice1")
+                Image("dice2")
+                }
+                .padding(.horizontal)
+                Spacer()
+                Button(action: {
+                }) {
+                    Text("Roll")
+                        .font(Font.custom("Noteworthy-Bold", size: 50))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                }
+                Spacer()
+            }
+        }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
